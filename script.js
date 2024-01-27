a92c92fb585c25ebb26464a9f8fb61bd40d820df
Melo La Mantia <carmelo.la.mantia00@gmail.com>
2024-01-28 00:20:26 +0100

    email fixed

--- script.js
+++ script.js
@@ -26,40 +26,22 @@ var TrandingSlider = new Swiper('.tranding-slider', {
   });
 
 // EMAILJS SEND
-// function sendMail() {
-//   console.log('Invio email...');
+function sendMail() {
+  event.preventDefault();
 
-//   let pixelProParams = {
-//     to_name: 'PixelPro',
-//     user_name:document.getElementById('user_name').value,
-//     email: document.getElementById('user_email').value,
-//     message: document.getElementById('message').value
-//   };
+  let pixelProParams = {
+    to_name: 'PixelPro',
+    user_name:document.getElementById('user_name').value,
+    email: document.getElementById('user_email').value,
+    message: document.getElementById('message').value
+  };
 
-//   console.log('Dati email:', pixelProParams);
+  emailjs.send('service_gt2uoev', 'template_n0ykqqo', pixelProParams)
+    .then(function(response){
+      alert('SUCCESS!', response.status, response.text);
+      document.getElementById('contact-form').reset();
+    }, function(error){
+      alert('FAILED, TRY AGAIN', error);
+    });
 
-//   emailjs.send('service_gt2uoev', 'template_n0ykqqo', pixelProParams)
-//     .then(function(response){
-//       alert('SUCCESS!', response.status, response.text);
-//       console.log('email inviata con successo');
-//     }, function(error){
-//       alert('FAILED, TRY AGAIN', error);
-//       console.log('suca');
-//     });
-
-// }
-
-// EMAILJS SUBMIT
-// window.onload = function() {
-//   document.getElementById('contact-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-    
-//     emailjs.sendForm('service_gt2uoev', 'template_n0ykqqo', '#contact-form')
-//     .then(function(response) {
-//        console.log('SUCCESS!', response.status, response.text);
-//     }, function(error) {
-//        console.log('FAILED...', error);
-//     });
-//   });
-// }
-
+}
